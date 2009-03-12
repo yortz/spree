d6614d89917b182c564f30f160c490d88ff8d256
paulcc <paulcc.two@gmail.com>
2009-03-12 14:11:05 +0000

    silly typos

--- vendor/extensions/payment_gateway/db/migrate/20090312133938_remove_redundant_protx_fields.rb
+++ vendor/extensions/payment_gateway/db/migrate/20090312133938_remove_redundant_protx_fields.rb
@@ -1,14 +1,12 @@
 class RemoveRedundantProtxFields < ActiveRecord::Migration
   def self.up
-    protx = Gateway.find_by_name('protx')
-    GatewayOption.find_by_gateway(protx).destroy
+    protx = Gateway.find_by_name('Protx')
+    GatewayOption.find_by_gateway_id(protx.id).destroy
     GatewayOption.create(:name => 'login', :description => 'Your Protx Vendor Name (note: remember to set the server IP addresses in your VSP account', :gateway => protx, :textarea => false)
   end
 
   def self.down
     # deliberately left blank because 'up' doesn't remove information
     # that is ever used in previous versions of the code.
-  end
-
   end
 end
