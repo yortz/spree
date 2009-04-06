18a919dcd40614b1542290f7dbf5b639180dbedf
paulcc <paulcc.two@gmail.com>
2009-04-06 10:08:03 +0100

    needed to ensure that OptionValues are cleared too from the old settings

--- vendor/extensions/payment_gateway/db/migrate/20090312133938_remove_redundant_protx_fields.rb
+++ vendor/extensions/payment_gateway/db/migrate/20090312133938_remove_redundant_protx_fields.rb
@@ -1,7 +1,8 @@
 class RemoveRedundantProtxFields < ActiveRecord::Migration
   def self.up
     protx = Gateway.find_by_name('Protx')
-    GatewayOption.find_by_gateway_id(protx.id).destroy
+    GatewayOption.find_all_by_gateway_id(protx.id).each {|o| o.destroy }    # also clears option values
+
     GatewayOption.create(:name => 'login', :description => 'Your Protx Vendor Name (note: remember to set the server IP addresses in your VSP account', :gateway => protx, :textarea => false)
   end
 
